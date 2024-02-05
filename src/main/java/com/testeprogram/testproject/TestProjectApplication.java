3e6ec11124e6d7f19706c13b0866b7e11aa51fcf
Kaua Vinicios <kaua.vinicios_2005@hotmail.com>
2024-02-05 18:55:10 -0300

    teste junior

--- src/main/java/com/testeprogram/testproject/TestProjectApplication.java
+++ src/main/java/com/testeprogram/testproject/TestProjectApplication.java
@@ -8,7 +8,7 @@ public class TestProjectApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TestProjectApplication.class, args);
-		System.out.println("Hello World");
+		System.out.println("Hello WorlD");
 	}
 
 }
